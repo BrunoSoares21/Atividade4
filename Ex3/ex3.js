1ce4f9548dd6c2bd07ceab3d568c109d06235199
Bruno Soares <brunosoaresdasilva25@gmail.com>
2023-09-03 11:21:41 -0300

    Atualizando ex3.js

--- Ex3/ex3.js
+++ Ex3/ex3.js
@@ -2,32 +2,32 @@ import PromptSync from "prompt-sync"
 
 const prompt = PromptSync()
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+function encontrarMaiorMenor(listaDeNumeros) {
+  if (listaDeNumeros.length === 0) {
+    console.log("A lista está vazia.");
+    return;
+  }
 
-if (numbers.length === 0) {
-    return {
-      maior: null,
-      menor: null,
+  let maior = listaDeNumeros[0];
+  let menor = listaDeNumeros[0];
+
+  for (let i = 1; i < listaDeNumeros.length; i++) {
+    if (listaDeNumeros[i] > maior) {
+      maior = listaDeNumeros[i];
     }
+    if (listaDeNumeros[i] < menor) {
+      menor = listaDeNumeros[i];
+    }
+  }
+
+  console.log("O maior valor é: " + maior);
+  console.log("\nO menor valor é: " + menor);
 }
 
-let maior = numbers[0];
-let menor = numbers[0];
+console.log("Esse código mostra o maior e menor valor de uma lista pré definida no sistema!\n")
 
-for (const number of numbers) {
-    if (number > maior) {
-      maior = number;
-    }
-    if (number < menor) {
-      menor = number;
-    }
-}
-
-return {
-    maior,
-    menor,
-}
-
+const listaDeNumeros = [20, 62, 6, 920, -5, 0];
+encontrarMaiorMenor(listaDeNumeros);
 
 console.log(`Maior valor: ${maior}`)
 console.log(`Menor valor: ${menor}`)
