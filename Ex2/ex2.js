57f81194ea7692299a384702c76a8599a82e151f
Bruno Soares <brunosoaresdasilva25@gmail.com>
2023-09-02 21:36:02 -0300

    atualizando ex2.js

--- Ex2/ex2.js
+++ Ex2/ex2.js
@@ -5,15 +5,15 @@ const prompt = PromptSync()
 function tabuada(num, limite) {
     
     for (let i = 1; i <= limite; i++) {
-        const total = numDigitado * i
-        console.log(`${numDigitado} X ${i} = ${total}`)
+        const total = num * i
+        console.log(`${num} X ${i} = ${total}`)
     }
-
+    
 }
 
-const num = numDigitado(prompt('Informe um número para para saber sua tabuada: '))
+const num = prompt('Informe um número para para saber sua tabuada: ')
 
-const limite = limiteDigitado(prompt('Informe um número limite para realizar a tabuada: '))
+const limite = prompt('Informe um número limite para realizar a tabuada: ')
 
 const executarTabuada = tabuada(num, limite)
 
