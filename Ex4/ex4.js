e062d0c47379868d03380ce4737e2a25c193c181
Bruno Soares <brunosoaresdasilva25@gmail.com>
2023-09-07 10:22:55 -0300

    Atualizando código ex4.js

--- Ex4/ex4.js
+++ Ex4/ex4.js
@@ -1,4 +1,38 @@
 import PromptSync from "prompt-sync"
+import readline from "readline"
 
 const prompt = PromptSync()
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function ColetarDadosCliente() {
+  rl.question("Digite o nome do cliente (deixe em branco para sair): ", function(nome) {
+    if (nome === "") {
+      console.log(`\nEncerrando programa!\n`)
+      rl.close()
+    } else {
+      ColetarHobbies(nome, [])
+    }
+  })
+}
+
+function ColetarHobbies(nome, hobbies) {
+  rl.question("\nDigite um hobby (deixe em branco para sair da lista): ", function(hobby) {
+    if (hobby === "") {
+      const hobbiesFormatados = hobbies.join(", ")
+      console.log(`\nNome: ${nome} | Hobbies: ${hobbiesFormatados}\n`)
+      ColetarDadosCliente()
+    } else {
+      hobbies.push(hobby)
+      ColetarHobbies(nome, hobbies)
+    }
+  })
+}
+
+console.log(`\nEste programa coleta dados do cliente, como o Nome e o Hobby!\n`)
+
+ColetarDadosCliente()
 
